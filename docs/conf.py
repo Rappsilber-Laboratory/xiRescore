6927578ea5c29e2915506c7044be31117b6671b5
Falk B. Schimweg <git@falk.schimweg.de>
2024-07-23 22:10:56 +0200

    Change copyright

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ import xirescore
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'xiRescore'
-copyright = "2024, Falk Boudewijn Schimweg"
+copyright = "2024, Rappsilber Laboratory"
 author = "Falk Boudewijn Schimweg"
 
 # The version info for the project you're documenting, acts as replacement
@@ -64,7 +64,7 @@ release = xirescore.__version__
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -83,7 +83,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
