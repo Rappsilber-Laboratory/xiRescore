777350ca2d07c3936828210e5ce2747b6ea01ea1
Falk B. Schimweg <git@falk.schimweg.de>
2024-07-26 14:08:41 +0200

    Add CLI test

--- tests/test_xirescore.py
+++ tests/test_xirescore.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 """Tests for `xirescore` package."""
-
+import pandas as pd
 import pytest
 import logging
 import tempfile
+import subprocess
+import os
 
 from xirescore.XiRescore import XiRescore
 
@@ -26,8 +28,11 @@ def test_full_db_rescoring():
 @pytest.mark.parquet
 def test_full_parquet_rescoring():
     with tempfile.TemporaryDirectory(prefix='pytest_xirescore_') as tmpdirname:
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        )
         logger = logging.getLogger(__name__)
-        logging.basicConfig()
         logger.setLevel(logging.DEBUG)
         logger.info('Start full parquet rescoring test')
         logger.info(f'Write results to {tmpdirname}')
@@ -59,5 +64,123 @@ def test_full_parquet_rescoring():
         rescorer.run()
 
 
-def test_cli():
-    raise Exception('Not implemented yet.')
+@pytest.mark.csv
+def test_full_csv_rescoring():
+    with tempfile.TemporaryDirectory(prefix='pytest_xirescore_') as tmpdirname:
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        )
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.DEBUG)
+        logger.info('Start full CSV rescoring test')
+        logger.info(f'Write results to {tmpdirname}')
+
+        options = {
+            'input': {
+                'columns': {
+                    'features': [
+                        'match_score',
+                        'better_score',
+                        'worse_score',
+                        'useless_score_uni',
+                        'useless_score_norm',
+                        'conditional_score',
+                    ]
+                }
+            },
+            'rescoring': {
+                'spectra_batch_size': 25_000  # Rescore in 4 batches
+            }
+        }
+
+        rescorer = XiRescore(
+            input_path='./fixtures/test_data.csv.gz',
+            output_path=f'{tmpdirname}/result.csv.gz',
+            options=options,
+            logger=logger,
+        )
+        rescorer.run()
+
+
+@pytest.mark.df
+def test_full_df_rescoring():
+    df = pd.read_parquet('./fixtures/test_data.parquet')
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    )
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    logger.info('Start full DF rescoring test')
+
+    options = {
+        'input': {
+            'columns': {
+                'features': [
+                    'match_score',
+                    'better_score',
+                    'worse_score',
+                    'useless_score_uni',
+                    'useless_score_norm',
+                    'conditional_score',
+                ]
+            }
+        },
+        'rescoring': {
+            'spectra_batch_size': 25_000  # Rescore in 4 batches
+        }
+    }
+
+    rescorer = XiRescore(
+        input_path=df,
+        options=options,
+        logger=logger,
+    )
+    rescorer.run()
+    df_out = rescorer.get_rescored_output()
+    assert len(df) == len(df_out)
+
+
+def test_full_cli_parquet_rescoring():
+    with tempfile.TemporaryDirectory(prefix='pytest_xirescore_') as tmpdirname:
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        )
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.DEBUG)
+        logger.info('Start full CSV rescoring test')
+        logger.info(f'Write results to {tmpdirname}')
+
+        options = {
+            'input': {
+                'columns': {
+                    'features': [
+                        'match_score',
+                        'better_score',
+                        'worse_score',
+                        'useless_score_uni',
+                        'useless_score_norm',
+                        'conditional_score',
+                    ]
+                }
+            },
+            'rescoring': {
+                'spectra_batch_size': 25_000  # Rescore in 4 batches
+            }
+        }
+
+        result = subprocess.run(
+            [
+                'xirescore',
+                '-i', './fixtures/test_data.parquet',
+                '-o', f'{tmpdirname}/result.parquet',
+                '-C', str(options),
+            ],
+            capture_output=True,
+            text=True
+        )
+
+        assert result.returncode == 0, f"CLI command failed with error: {result.stderr}"
+        assert os.path.exists(f'{tmpdirname}/result.parquet'), f"Output file was not created."
