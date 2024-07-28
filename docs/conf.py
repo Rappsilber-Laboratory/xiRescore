c96ea24159ee6811e4aa56ea617f5234f11aef9e
Falk B. Schimweg <git@falk.schimweg.de>
2024-07-28 17:48:50 +0200

    Add autosummary

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,7 @@ import xirescore
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
     'sphinx.ext.viewcode',
     'sphinx_rtd_theme'
 ]
