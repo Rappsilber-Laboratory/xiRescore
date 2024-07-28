e94595ef58d63e876ccda3b2b3687e7b5c9557eb
Falk B. Schimweg <git@falk.schimweg.de>
2024-07-28 17:49:48 +0200

    Change version to alpha

--- setup.py
+++ setup.py
@@ -80,6 +80,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/z3rone/xirescore',
-    version='0.1.0',
+    version='1.0.0a1',
     zip_safe=False,
 )
