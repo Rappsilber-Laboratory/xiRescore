c427b87c09417eda1c28b0b7bbf889ce67cdb354
Falk B. Schimweg <f.schimweg@win.tu-berlin.de>
2024-07-30 12:20:12 +0200

    Add logo

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.viewcode',
-    'sphinx_rtd_theme'
+    'sphinx_rtd_theme',
+    'sphinx_favicon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -95,13 +96,20 @@ html_theme = 'sphinx_rtd_theme'
 # theme further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+  'logo_only': True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+favicons = [
+  {"href": "logo.svg"},
+]
+
+html_logo = "_static/logo.svg"
 
 # -- Options for HTMLHelp output ---------------------------------------
 
