1e25f502859aa0ded23601d960d06d497ddeedd6
Falk B. Schimweg <git@falk.schimweg.de>
2024-08-07 19:54:53 +0200

    Improve logging

--- xirescore/XiRescore.py
+++ xirescore/XiRescore.py
@@ -232,8 +232,8 @@ class XiRescore:
                 logger=self._logger,
                 random_seed=self._true_random()
             )
-            self._logger.info(f'Batch contains {len(df_batch)} samples')
-            self._logger.debug(f'Batch uses approx. {df_batch.memory_usage().sum()/1024/1024:.2f}MB of RAM')
+            self._logger.info(f'Batch contains {len(df_batch):,.0f} samples')
+            self._logger.debug(f'Batch uses approx. {df_batch.memory_usage().sum()/1024/1024:,.2f}MB of RAM')
 
             # Rescore batch
             df_batch = self.rescore_df(df_batch)
