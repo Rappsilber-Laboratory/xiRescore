33d39b62d63cf676dccf3f418d24555d89e49525
Falk B. Schimweg <git@falk.schimweg.de>
2024-08-08 13:48:40 +0200

    Switch license

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.10',
