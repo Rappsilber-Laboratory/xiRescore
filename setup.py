8d13741e57825603e0b0bb1441cf0ec436251b5c
z3rone <git@falk.schimweg.de>
2024-08-26 09:59:31 +0200

    Add python 3.11

--- setup.py
+++ setup.py
@@ -84,6 +84,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
     ],
     description="A machine learning based approach to rescore crosslinked spectrum matches (CSMs).",
